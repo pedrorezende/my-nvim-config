d0672e0457f1aeee361e458c1e6f7ad6f42749a3
Pedro Rezende <xpedrorezende@gmail.com>
2024-05-01 02:57:46 -0300

    feat: adding extra keymaps and separating file

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
+--
 -- https://github.com/nvim-lua/kickstart.nvim
+--
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
@@ -26,20 +29,7 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- Diagnostic keymaps
-vim.keymap.set("n", "[e", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]e", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-
--- Keybinds to make split navigation easier.
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
+require("keymaps")
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
---
+++ lua/keymaps.lua
@@ -0,0 +1,28 @@
+local keymaps = vim.keymap
+
+-- Increment / Decrement
+keymaps.set("n", "+", "<C-a>")
+keymaps.set("n", "-", "<C-x>")
+
+-- Leave insert mode
+keymaps.set("i", "kk", "<Esc>")
+keymaps.set("i", "jj", "<Esc>")
+
+-- Adding line without leaving normal mode
+keymaps.set("n", "zo", "o<Esc>k")
+keymaps.set("n", "zO", "O<Esc>j")
+
+-- Select all
+keymaps.set("n", "<C-a>", "gg<S-v>G")
+
+-- Diagnostic keymaps
+keymaps.set("n", "[e", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+keymaps.set("n", "]e", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+keymaps.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+keymaps.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- Keybinds to make split navigation easier.
+keymaps.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+keymaps.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+keymaps.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+keymaps.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
