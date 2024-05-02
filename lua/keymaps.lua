584c9db2223c24162bf36249c376269c2359a72c
Pedro Rezende <xpedrorezende@gmail.com>
2024-05-02 01:15:52 -0300

    feat: adding zenmode plugin

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -26,3 +26,5 @@ keymaps.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window"
 keymaps.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 keymaps.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 keymaps.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+keymaps.set("n", "<leader>z", "<cmd>ZenMode<CR>")
