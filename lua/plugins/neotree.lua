3046fd1cec362ee3100725c39818995857f89221
Pedro Rezende <xpedrorezende@gmail.com>
2024-05-02 01:21:56 -0300

    feat: showing hidden files

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -49,6 +49,18 @@ return {
 			bind_to_cwd = false,
 			follow_current_file = { enabled = true },
 			use_libuv_file_watcher = true,
+			filtered_items = {
+				visible = true,
+				show_hidden_count = true,
+				hide_dotfiles = false,
+				hide_gitignored = true,
+				hide_by_name = {
+					".git",
+					".DS_Store",
+					"thumbs.db",
+				},
+				never_show = {},
+			},
 		},
 		window = {
 			position = "float",
