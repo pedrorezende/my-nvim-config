fa6dd08a5aef8b4b746c596a2cf4476c01ae37d9
Pedro Rezende <xpedrorezende@gmail.com>
2024-06-01 10:35:12 +0200

    feat: adding elixir lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -169,6 +169,9 @@ return { -- LSP Configuration & Plugins
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
 			rust_analyzer = {},
+
+			elixirls = {},
+
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
 			-- Some languages (like typescript) have entire language plugins that can be useful:
